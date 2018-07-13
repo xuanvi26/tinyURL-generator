fe79cf0e776b729723941205134dde78ffae55ae
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-07-13 15:04:09 -0400

    Add package.json

--- expressServer.js
+++ expressServer.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; 
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.set("view engine", "ejs");
+
 const URLDatabase = {};
 const users = {};
 
