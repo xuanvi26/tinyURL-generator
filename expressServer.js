211890c663a53787bb4c33ddd7bec12208606277
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-07-13 14:50:50 -0400

    Generate random string and create URLdatabase and users

---
+++ expressServer.js
@@ -0,0 +1,15 @@
+const URLDatabase = {};
+const users = {};
+
+const generateRandomString = () => {
+    let uniqueSURL = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 6; i++) {
+        uniqueSURL += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return uniqueSURL;
+}
+
+
+
+
