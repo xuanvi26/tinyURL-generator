9580ec7bf844c3ea8bc76ee071033a033479aab5
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-07-14 10:58:55 -0400

    Redirect to login if no cookie

--- expressServer.js
+++ expressServer.js
@@ -29,7 +29,7 @@ const generateRandomString = () => {
     return uniqueSURL;
 }
 
-const redirectLogin = () => {
+const redirectLogin = (res) => {
     if (!res.cookie["user_id"]) res.redirect('/login');
 }
 
@@ -40,14 +40,17 @@ app.get("/", (req, res) => {
 
 //LOGIN
 app.get("/login", (req, res) => {
+    res.sendStatus(200);
 })
 
 // REGISTER
 app.get("/register", (req, res) => {
+    redirectLogin(res);
 })
 
 // VIEW ALL URLS
 app.get("/viewURLs", (req, res) => {
+    redirectLogin(res);
 })
 
 // LOGOUT
